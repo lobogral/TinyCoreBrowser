588226be8853422d6f816b9ea6113f66412686de
Camilo Sabogal <lobogral@gmail.com>
2021-01-06 17:38:53 -0500

    Update Instalador.sh

--- Instalador.sh
+++ Instalador.sh
@@ -1,4 +1,3 @@
 sed -i '1c\http://distro.ibiblio.org/tinycorelinux/' /opt/tcemirror
 tce-load -wi tc-install.tcz
 sudo tc-install.sh /mnt/sr0/boot/core.gz frugal sda 0 ext4 no no X none
-sudo reboot
