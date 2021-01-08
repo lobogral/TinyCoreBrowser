435e5c0855821f9a04ffe7603983a4cbea826e00
Camilo Sabogal <lobogral@gmail.com>
2021-01-08 15:39:03 -0500

    Update Preparador.sh

--- Preparador.sh
+++ Preparador.sh
@@ -1,20 +1,25 @@
 ####################################################################################
 # Actualizaciones
 ####################################################################################
+# Interfaz grafica
+tce-load -wi Xvesa aterm flwm_topside wbar
 # Repositorio de posibles instalaciones
 sed -i '1c\http://distro.ibiblio.org/tinycorelinux/' /opt/tcemirror
 # Resolusion de pantalla
 sed -i 's/1024x768/800x600/' /home/tc/.xsession
 # Teclado latino
-tce-load -wi kmaps.tcz
+tce-load -wi kmaps
 sudo sed -i '$a loadkmap < /usr/share/kmap/qwerty/la-latin1.kmap' /opt/bootlocal.sh
 # Navegador
-tce-load -wi minefield21.tcz
+tce-load -wi minefield21
 # Sonido
-tce-load -wi alsa.tcz
-tce-load -wi alsa-config.tcz
+tce-load -wi alsa alsa-config
 alsactl init
 alsactl store
 sudo sed -i '$a alsactl restore' /opt/bootlocal.sh
 # Flash
-tce-load -wi getFlash_beta.tcz
+tce-load -wi getFlash_beta
+# Guardo todo
+echo "y" | backup
+# Reiniciar
+sudo reboot
